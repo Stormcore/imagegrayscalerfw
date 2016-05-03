338df5e635f12d1e128e158cadae77982f91812d
Dzmitry Sukhau <dzmitrysukhau@dzmitrys-imac.local>
2016-05-03 18:37:04 +0300

    Initial Commit

---
+++ ImageProcessor/ImageProcessor.h
@@ -0,0 +1,19 @@
+//
+//  ImageProcessor.h
+//  ImageProcessor
+//
+//  Created by Dzmitry Sukhau on 5/3/16.
+//  Copyright © 2016 Dzmitry Sukhau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageProcessor.
+FOUNDATION_EXPORT double ImageProcessorVersionNumber;
+
+//! Project version string for ImageProcessor.
+FOUNDATION_EXPORT const unsigned char ImageProcessorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageProcessor/PublicHeader.h>
+
+
